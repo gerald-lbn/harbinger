9ead7e0aefa7f76a126025af2de5d16d131a2685
Gérald Lbn <gerald.leban0@gmail.com>
2026-04-16 14:57:10 +0200

    chore: remove unused method from feed controller

--- apps/backend/app/controllers/feeds_controller.ts
+++ apps/backend/app/controllers/feeds_controller.ts
@@ -4,21 +4,6 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class FeedsController {
   /**
-   * Display a list of resource
-   */
-  async index({}: HttpContext) {}
-
-  /**
-   * Display form to create a new record
-   */
-  async create({}: HttpContext) {}
-
-  /**
-   * Handle form submission for the create action
-   */
-  async store({ request }: HttpContext) {}
-
-  /**
    * Show individual record
    */
   async show({ params, response, serialize }: HttpContext) {
@@ -27,19 +12,4 @@ export default class FeedsController {
 
     return serialize(FeedTransformer.transform(feed))
   }
-
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
-
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params }: HttpContext) {}
-
-  /**
-   * Delete record
-   */
-  async destroy({ params }: HttpContext) {}
 }
