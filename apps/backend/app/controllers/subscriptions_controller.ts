a3192f2629bc23a165cd60cd54ee5fb0855447fa
Gérald Lbn <gerald.leban0@gmail.com>
2026-04-16 15:29:59 +0200

    fix: preload feed with subscription

--- apps/backend/app/controllers/subscriptions_controller.ts
+++ apps/backend/app/controllers/subscriptions_controller.ts
@@ -7,38 +7,42 @@ export default class SubscriptionsController {
    */
   async index({ auth, serialize }: HttpContext) {
     const user = auth.getUserOrFail()
-    const subscriptions = await user.related('subscriptions').query().orderBy('created_at', 'desc')
+    const subscriptions = await user
+      .related('subscriptions')
+      .query()
+      .preload('feed')
+      .orderBy('created_at', 'desc')
 
-    return serialize(subscriptions.map((sub) => SubscriptionTransformer.transform(sub)))
+    return serialize(SubscriptionTransformer.transform(subscriptions))
   }
 
   /**
    * Display form to create a new record
    */
-  async create({}: HttpContext) {}
+  async create({ }: HttpContext) { }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request }: HttpContext) { }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) { }
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  async edit({ params }: HttpContext) { }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) { }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params }: HttpContext) { }
 }
