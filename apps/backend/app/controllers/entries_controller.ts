27c776f8fee97aef8b87d00e069031f7918be394
Gérald Leban <gerald.leban0@gmail.com>
2026-04-24 13:41:57 +0200

    fix: remove empty controller methods

--- apps/backend/app/controllers/entries_controller.ts
+++ apps/backend/app/controllers/entries_controller.ts
@@ -5,7 +5,7 @@ import { feedEntriesPaginationValidator } from '#validators/entry'
 
 @inject()
 export default class EntriesController {
-  constructor(private entryService: EntryService) { }
+  constructor(private entryService: EntryService) {}
 
   /**
    * Display a list of resource
@@ -23,34 +23,4 @@ export default class EntriesController {
 
     return entries
   }
-
-  /**
-   * Display form to create a new record
-   */
-  async create({ }: HttpContext) { }
-
-  /**
-   * Handle form submission for the create action
-   */
-  async store({ request }: HttpContext) { }
-
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) { }
-
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) { }
-
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params, request }: HttpContext) { }
-
-  /**
-   * Delete record
-   */
-  async destroy({ params }: HttpContext) { }
 }
