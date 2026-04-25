b582e6bc1d1a9e2fb0c8f8c65250be97c5c2ed5b
Gérald Leban <gerald.leban0@gmail.com>
2026-04-25 12:33:59 +0200

    chore: lint code and fix tsconfig issue

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -18,7 +18,12 @@ export default defineConfig(
 	prettier,
 	svelte.configs.prettier,
 	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
+			parserOptions: {
+				tsconfigRootDir: import.meta.dirname
+			}
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
@@ -32,7 +37,8 @@ export default defineConfig(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
+				svelteConfig,
+				tsconfigRootDir: import.meta.dirname
 			}
 		}
 	},
